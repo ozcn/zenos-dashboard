6a3591458452dd923ae0dd71723c8094a666cc09
Tadashi Shigeoka <shigeoka@bakorer.com>
2016-10-10 13:18:03 +0900

    特定の wallet address の情報を取得する API を実装

--- colu_api/index.js
+++ colu_api/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var request = require('request');
+var util = require('util');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('hello Colu API');
@@ -23,6 +30,53 @@ app.post('/get_address', function(req, res) {
     console.log(body);
 
     res.json(body);
+  });
+});
+
+// 特定の wallet address の情報を取得する
+// (例) 自分が今いくら assets を持っているか？
+// http://documentation.colu.co/#GetAddressInfo38
+app.post('/get_address_info', function(req, res) {
+  if (!req.body.address) {
+    return res.json({
+      status: 'ng'
+    });
+  }
+
+  var params = {
+    address: req.body.address
+  };
+
+  var jsonData = {
+    jsonrpc: "2.0", // mandatory
+    method: "coloredCoins.getAddressInfo", // mandatory
+    id: "1", // mandatory if response is needed
+    params: params // quary parameters
+  };
+
+  postToApi('', jsonData, function(err, body) {
+    if (err) {
+      console.error(err);
+    }
+
+    console.log(util.inspect(body, false, null));
+
+    var jsonResult = {
+      status: 'ok',
+      address: null,
+      assets: []
+    };
+
+    if (body.result) {
+      if (body.result.address) {
+        jsonResult.address = body.result.address;
+      }
+      if (body.result.utxos && body.result.utxos[0]) {
+        jsonResult.assets = body.result.utxos[0].assets;
+      }
+    }
+
+    res.json(jsonResult);
   });
 });
 
